5dbfe698f4014c896e528ce24da69382ce458d18
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2020-05-29 14:30:45 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -21,27 +21,26 @@ brew install curl
 brew install git
 brew install git-crypt
 brew install node
-brew install openssl
 brew install watchman
-brew install mongodb
 brew install postgres
 
 # Install GUI apps
+brew cask install 1password
 brew cask install google-chrome
+brew cask install firefox
 brew cask install visual-studio-code
 brew cask install vlc
 brew cask install slack
+brew cask install spotify
 brew cask install iterm2
 brew cask install tower
 brew cask install ngrok
 brew cask install alfred
 brew cask install bartender
-brew cask install dropbox
 brew cask install aerial
 brew cask install flycut
 brew cask install caffeine
 brew cask install appzapper
-brew cask install bar-magnet
 
 # Remove outdated versions from the cellar.
 brew cleanup
